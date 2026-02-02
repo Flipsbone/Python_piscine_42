e0642e059758e84869c5a155690d545c2a0d0ed9
Adrien Vacher <advacher@z2r10p7.42lyon.fr>
2026-02-02 16:55:49 +0100

    learn how to use dictionary and manage inventory

--- ex4/ft_inventory_system.py
+++ ex4/ft_inventory_system.py
@@ -4,26 +4,99 @@ using dictionary operations."""
 import sys
 
 
-def is_space(c: str) -> bool:
-    return (c == " " or c == "\t" or c == "\n")
+def display_dictionary(inventory: dict) -> None:
+    print(f"Dictionary keys: {list(inventory.keys())}")
+    print(f"Dictionary values: {list(inventory.values())}")
+    print(f"Sample lookup - 'sword' in inventory: {'sword' in inventory} ")
+
+
+def management_suggestion(inventory: dict) -> None:
+    restock = []
+    for item, qty in inventory.items():
+        if qty < 2:
+            restock.append(item)
+    print(f"Restock needed: {restock}")
+
+
+def rarity_item(inventory: dict) -> None:
+    categories = {
+        "Moderate": {},
+        "Scarce": {},
+    }
+    for item, qty in inventory.items():
+        if qty > 3:
+            categories["Moderate"][item] = qty
+        else:
+            categories["Scarce"][item] = qty
+    print(f"Moderate: {categories['Moderate']}")
+    print(f"Moderate: {categories['Scarce']}")
+
+
+def display_inventory(inventory: dict, total_item: int) -> None:
+    temp_inventory = inventory.copy()
+    while temp_inventory:
+        current_max = max(temp_inventory, key=temp_inventory.get)
+        value = temp_inventory[current_max]
+        try:
+            inventory_share = ((value / total_item)) * 100
+            print(f"{current_max}: {value} units {inventory_share:.1f}%")
+        except ZeroDivisionError as e:
+            print(f"Error : total_item {e}")
+        del temp_inventory[current_max]
+    print("\n=== Inventory Statistics ===")
+    top_item = max(inventory, key=inventory.get)
+    low_item = min(inventory, key=inventory.get)
+    print(f"Most abundant: {top_item} ({inventory[top_item]} units)")
+    print(f"Least abundant: {low_item} ({inventory[low_item]} units)")
+
+
+def count_total_item(inventory: dict) -> int:
+    return sum(inventory.values())
+
+
+def fill_out_inventory(inventory: dict) -> dict:
+    for arg in sys.argv[1:]:
+        key = ""
+        value_str = ""
+        parse = arg.split(":", 1)
+        if len(parse) == 2:
+            key = parse[0].strip().lower()
+            value_str = parse[1].strip()
+            try:
+                value_int = int(value_str)
+                current_val = inventory.get(key, 0)
+                value_int = current_val + value_int
+                inventory.update({key: value_int})
+            except ValueError:
+                print(f"Skipping invalid quantity must be a number:"
+                      f"'{value_str}' for item '{key}'")
+    return inventory
 
 
 if __name__ == "__main__":
     print("=== Inventory System Analysis ===")
-    inventory = {}
-    for arg in sys.argv[1:]:
-        key = ""
-        value_str = ""
-        word = True
-        for char in arg:
-            if is_space(char):
-                continue
-            if char == ":":
-                word = False
-                continue
-            if word is True:
-                key = key + char
-            else:
-                value_str = value_str + char
-            inventory.update({key: int(value_str)})
-    print(f"{inventory}")
+    if len(sys.argv) < 2:
+        print("No item provided. Usage: python3 ft_inventory_system.py "
+              "<item:number> <sword:1> ...")
+
+    if (len(sys.argv)) > 1:
+
+        inventory = {}
+
+        fill_out_inventory(inventory)
+
+        nb_unique_item = len(inventory)
+        total_item = count_total_item(inventory)
+
+        print(f"Total items in inventory: {total_item}")
+        print(f"Unique item types: {nb_unique_item}")
+
+        if total_item > 0:
+            print("\n=== Current Inventory ===")
+            display_inventory(inventory, total_item)
+            print("\n=== Item Categories ===")
+            rarity_item(inventory)
+            print("\n=== Management Suggestions ===")
+            management_suggestion(inventory)
+            print("\n=== Dictionary Properties Demo ===")
+            display_dictionary(inventory)
